299901a35ac8eaf0a3266c042abf4690a908adb7
MANOJ GOWDA VENKATACHALA MURTHY <manojgowdavenkatachalamurthy@MANOJs-MacBook-Air.local>
2023-08-13 19:05:02 -0400

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ManojGowda27/bnb-booking/pkg/config"
+	"github.com/ManojGowda27/bnb-booking/pkg/handlers"
+	"github.com/go-chi/chi"
+)
+
+func routes(app *config.AppConfig)http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handler.Repo.Home)
+	mux.Get("/about", handler.Repo.About)
+
+	return mux
+} 
